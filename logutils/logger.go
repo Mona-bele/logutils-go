7f408a1085525365188980dce71c2e66f9c2060b
Paulo Lopes Estevão <pl1745240@gmail.com>
2024-09-17 16:02:08 +0100

    refactor: Update logger initialization and configuration
    This commit refactors the logger initialization and configuration in the `InitLogger` function. It introduces a `Fields` type to handle additional log fields, and updates the logger output format to JSON. The log file path can now be modified based on the environment, and the log file is opened and set as the output if a path is provided. Otherwise, the standard output (stdout) is used. The log level can also be modified based on the environment. Additionally, the `Info`, `Error`, `Debug`, and `Warn` functions have been updated to handle the new `Fields` type.

--- logutils/logger.go
+++ logutils/logger.go
@@ -8,8 +8,34 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+type Fields map[string]interface{}
+
 // InitLogger configures the logger with a standardized format and log level.
 func InitLogger() {
+
+	// Set Zerolog's output format to JSON
+	zerolog.TimeFieldFormat = time.RFC3339
+
+	// Log file path: can be modified based on the environment
+	logFilePath := os.Getenv("LOG_PATH")
+	if logFilePath != "" {
+
+		logFilePath = "/var/log/" + logFilePath
+
+		// Open the log file
+		f, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to open log file")
+		}
+
+		// Set the log file as the output
+		log.Logger = zerolog.New(f).With().Caller().Timestamp().Logger()
+
+	} else {
+		// Standard output (stdout) to be captured by the container logging driver
+		// Log to stdout
+		log.Logger = zerolog.New(os.Stdout).With().Caller().Logger()
+	}
 
 	// Log level: can be modified based on the environment (debug, info, etc.)
 	LogLevel := os.Getenv("LOG_LEVEL")
@@ -20,18 +46,13 @@ func InitLogger() {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
 
-	// Set Zerolog's output format to JSON
-	zerolog.TimeFieldFormat = time.RFC3339
-
-	// Standard output (stdout) to be captured
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
 }
 
 // Info logs an information message.
 func Info(message string, fields map[string]interface{}) {
 	event := log.Info().Timestamp()
 	for k, v := range fields {
-		event = event.Str(k, v.(string))
+		event = event.Interface(k, v)
 	}
 	event.Msg(message)
 }
@@ -40,7 +61,7 @@ func Info(message string, fields map[string]interface{}) {
 func Error(message string, err error, fields map[string]interface{}) {
 	event := log.Error().Err(err).Timestamp()
 	for k, v := range fields {
-		event = event.Str(k, v.(string))
+		event = event.Interface(k, v)
 	}
 	event.Msg(message)
 }
@@ -49,7 +70,7 @@ func Error(message string, err error, fields map[string]interface{}) {
 func Debug(message string, fields map[string]interface{}) {
 	event := log.Debug().Timestamp()
 	for k, v := range fields {
-		event = event.Str(k, v.(string))
+		event = event.Interface(k, v)
 	}
 	event.Msg(message)
 }
@@ -58,16 +79,16 @@ func Debug(message string, fields map[string]interface{}) {
 func Warn(message string, fields map[string]interface{}) {
 	event := log.Warn().Timestamp()
 	for k, v := range fields {
-		event = event.Str(k, v.(string))
+		event = event.Interface(k, v)
 	}
 	event.Msg(message)
 }
 
 // Fatal logs a critical error message and terminates the program.
-func Fatal(message string, err error, fields map[string]interface{}) {
+func Fatal(message string, err error, fields Fields) {
 	event := log.Fatal().Err(err).Timestamp()
 	for k, v := range fields {
-		event = event.Str(k, v.(string))
+		event = event.Interface(k, v)
 	}
 	event.Msg(message)
 	os.Exit(1)
